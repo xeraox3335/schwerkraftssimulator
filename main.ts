ef728829531a64eb3ed1c8fccbf40184aa7c3ccd
Lennart Schmehr <lennart.schmehr@proton.me>
2026-02-24 09:19:12 +0100

    1.1.0

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-input.onGesture(Gesture.TiltRight, function () {
-    basic.showIcon(IconNames.ArrowWest)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    basic.showIcon(IconNames.ArrowEast)
-})
-input.onGesture(Gesture.LogoDown, function () {
-    basic.showIcon(IconNames.ArrowNorth)
-})
-input.onGesture(Gesture.LogoUp, function () {
-    basic.showIcon(IconNames.ArrowSouth)
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        basic.showIcon(IconNames.ArrowNorth)
+    } else if (input.isGesture(Gesture.LogoDown)) {
+        basic.showIcon(IconNames.ArrowSouth)
+    } else if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showIcon(IconNames.ArrowEast)
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        basic.showIcon(IconNames.ArrowWest)
+    } else if (input.isGesture(Gesture.ScreenDown)) {
+        basic.showIcon(IconNames.Diamond)
+    } else {
+        basic.showIcon(IconNames.Heart)
+    }
 })
