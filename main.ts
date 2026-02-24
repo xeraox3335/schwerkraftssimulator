78c34bcc2ff6a1384359b83205389a67fffb5267
Lennart Schmehr <lennart.schmehr@proton.me>
2026-02-24 09:12:24 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.ArrowWest)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.ArrowEast)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.ArrowNorth)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.ArrowSouth)
+})
